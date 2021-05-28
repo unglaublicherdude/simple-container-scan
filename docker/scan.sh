f9d2e776dc5c043743d6deaf2599be98d8b15b50
Simonis, Matthias <Matthias.Simonis@gdata.de>
2021-05-28 13:12:23 +0200

    ignore errors

--- docker/scan.sh
+++ docker/scan.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-clamscan --recursive=yes --suppress-ok-results --no-summary --allmatch=yes --detect-pua=yes /toscan | \
+clamscan --recursive=yes --suppress-ok-results --no-summary --allmatch=yes --detect-pua=yes /toscan 2>/dev/null | \
     grep -v ": Symbolic link" | grep -v ": Empty file" | grep -v ": Sanesecurity.Foxhole.Zip_exe.UNOFFICIAL FOUND" | \
     grep -v "decompress file size exceeds limits"
